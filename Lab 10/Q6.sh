c26112501cfb1c467d83574b8a3be05abb758ae3
RohitDhanjee <145933724+RohitDhanjee@users.noreply.github.com>
2024-01-02 21:07:36 +0500

    Q6.sh

---
+++ Lab 10/Q6.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+count=1
+
+#Part1
+# while [ $# -gt 0 ]; do
+#   echo "Parameter #count: $1"
+#   shift
+#   ((count++))
+# done
+
+#Part2
+# until [ $# -eq 0 ]; do
+#     echo "Parameter #count: $1"
+#     shift
+#     ((count++))
+# done
